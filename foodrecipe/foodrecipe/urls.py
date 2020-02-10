bf6d38bb33d1ef76e5ac85d90f406b7fe09a0f37
tenzinsparkss <58834694+tenzinsparkss@users.noreply.github.com>
2020-02-10 10:15:41 +0545

    Update urls.py

--- foodrecipe/foodrecipe/urls.py
+++ foodrecipe/foodrecipe/urls.py
@@ -14,10 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
-    path('',include('main.urls')),
-    path('',include('user_profile.urls')),
+    path('', include('main.urls')),
+    path('', include('feedback.urls')),
+    path('', include('restapi.urls')),
+    path('', include('recipe_download.urls')),
     path('admin/', admin.site.urls),
-]
+    
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
